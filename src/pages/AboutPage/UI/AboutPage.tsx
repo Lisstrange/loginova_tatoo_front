be974e8859f43be3b3af38ff03411686e78236eb
Lisstrange <nik.lis@bk.ru>
2023-07-20 00:06:46 +0300

    style flex about page

--- src/pages/AboutPage/UI/AboutPage.tsx
+++ src/pages/AboutPage/UI/AboutPage.tsx
@@ -5,9 +5,20 @@ import { FileSlider } from "@/features/FileSlider";
 
 const AboutPage: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <div className={styles.fileslider}>
-        <FileSlider />
+        <div className={styles.slider}>
+          <FileSlider />
+        </div>
+
+        <div className={styles.technics}>
+          Чем я занимаюсь:
+          <div> - Тату в стиле реализм</div>
+          <div> - Кастом одежды и обуви</div>
+          <div> - Реставрация аксессуаров</div>
+          <div> - Проектирование ландшафтного дизайна</div>
+          <div> - Тату в стиле реализм</div>
+        </div>
       </div>
     </div>
   );
